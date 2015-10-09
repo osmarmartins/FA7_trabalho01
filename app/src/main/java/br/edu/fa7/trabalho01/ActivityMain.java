8b3ada8d1c6ba6eac4eb8fe2121961bfdcefa781
Osmar Martins Filho <osmarfilho6@gmail.com>
2015-10-09 14:28:45 -0300

    Layout da app

--- app/src/main/java/br/edu/fa7/trabalho01/ActivityMain.java
+++ app/src/main/java/br/edu/fa7/trabalho01/ActivityMain.java
@@ -2,20 +2,39 @@ package br.edu.fa7.trabalho01;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by osmar on 08/10/2015.
  */
 public class ActivityMain extends Activity {
 
+    private Button btnAndroidWeb;
+    private Button btnEspelho;
+    private Button btnLigar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Associando os componentes da View.
+        btnAndroidWeb = (Button) findViewById(R.id.btnAndroidWeb);
+        btnEspelho = (Button) findViewById(R.id.btnEspelho);
+        btnLigar = (Button) findViewById(R.id.btnLigar);
 
 
 
+        // Uso de método anônimo para simplificar o desenvolvimento.
+        btnAndroidWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
 
     }
 }
